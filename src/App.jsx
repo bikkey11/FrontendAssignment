28375de59509767b66fa3e2b2bb377e8cd45e1d1
bikkey11 <bikashyogi0@gmail.com>
2025-07-11 17:58:07 +0545

    First commit

---
+++ src/App.jsx
@@ -0,0 +1,9 @@
+import Task1 from "./components/task1/task1";
+
+export default function App() {
+  return (
+   <>
+   <Task1/>
+   </>
+  )
+}
---
+++ src/components/task1/task1.jsx
@@ -0,0 +1,11 @@
+import React from 'react'
+
+const Task1 = () => {
+    return (
+        <div className='text-4xl'>
+            Task1
+        </div>
+    )
+}
+
+export default Task1
